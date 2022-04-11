6058faac1eb32e6ecfa7b9cde6482fec6cff4ba7
Shane <shanerichter09@yahoo.com>
2022-04-10 21:47:45 -0400

    updated unhinderd link

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -11,7 +11,7 @@ function Portfolio() {
 
       <PortfolioPiece images={["/github.png"]} title="Github" description="My github profile" link="https://github.com/Shaner09"/>
 
-      <PortfolioPiece images={["/unhinderdpic3.jpeg", "/unhinderdpic2.jpeg", "/unhinderdpic1.jpeg"]} title="unhinderd" description="unhinderd is a messaging app that handles translations behind the scenes for seamless communication between people who would otherwise have a difficult time speaking to eachother. This app uses the google translate api to translate to and from over 100 different languages! This app was an origional design created by myself and 3 other developers." link="https://unhinderd.herokuapp.com/"></PortfolioPiece>
+      <PortfolioPiece images={["/unhinderdpic3.jpeg", "/unhinderdpic2.jpeg", "/unhinderdpic1.jpeg"]} title="unhinderd" description="unhinderd is a messaging app that handles translations behind the scenes for seamless communication between people who would otherwise have a difficult time speaking to eachother. This app uses the google translate api to translate to and from over 100 different languages! This app was an origional design created by myself and 3 other developers." link="https://unhindrdclient.vercel.app/"></PortfolioPiece>
 
       <PortfolioPiece images={["/challenge1.png", "/challenge2.png", "/challenge3.png"]} title="Kenzie Challenges" description="Kenzie Challenges is a website I made to house some of the last assignments that I completed with kenzie. My favorite challenge was tic-tac-toe, where I created a computer player that people have a hard time beating, but there is one sequence that you can use to win! can you find it?" link="challenges"/>
     </div>
